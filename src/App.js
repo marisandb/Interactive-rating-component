a9ba047b7b2e2432684c53b891358db3c261ae71
maris <marisandb@yahoo.com>
2022-06-10 15:28:15 -0500

    added basic HTML for rating-thank-you page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Rating from "./pages/rating"
+import RatingThankYou from "./pages/rating-thank-you"
+
 function App() {
   return (
     <div className="App">
      <Rating />
+     <RatingThankYou />
     </div>
   );
 }
---
+++ src/pages/rating-thank-you.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function RatingThankYou() {
+  return (
+    <section id="rating">
+        <h1>You selected X out of 5</h1>
+        <h2>Thank you!</h2>
+        <p> We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
+    </section>
+  );
+}
+
+export default RatingThankYou; 
