db2eda7e0c6baa5d2fdad7033d4503cef09dfc22
maris <marisandb@yahoo.com>
2022-06-10 15:23:59 -0500

    added basic HTML for rating page

---
+++ src/pages/rating.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Rating() {
+  return (
+    <section id="rating">
+        <h1>How did we do?</h1>
+
+        <p> Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+
+
+        <button>Submit</button>
+    </section>
+  );
+}
+
+export default Rating; 
