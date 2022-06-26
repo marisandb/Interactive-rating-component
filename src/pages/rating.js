bc6d08ba1bc8e5fbcf577f187d5d1e1fec6513ca
maris <marisandb@yahoo.com>
2022-06-26 01:04:51 -0500

    added on click and hover to main page

--- src/pages/rating.js
+++ src/pages/rating.js
@@ -2,22 +2,30 @@ import React from "react";
 import icon from "../images/icon-star.svg"
 
 function Rating() {
-  return (
-    <section id="rating">
-        <img src={icon} alt="star"/> 
-        <h1>How did we do?</h1>
+  const handleClick = e => {
+    e.currentTarget.style.backgroundColor = `var(--Primary)`;
+    e.currentTarget.style.color = 'white';
+  };
+  const handleSubmit = e => {
+    e.currentTarget.style.backgroundColor = `var(--White)`;
+    e.currentTarget.style.color = `var(--Primary)`;
+  };
 
+  return (
+    <section id="rating" className="container">
+        <img src={icon} className="icon" alt="star"/> 
+        <h1>How did we do?</h1>
         <p> Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
         <div className="rating-buttons">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
+        <button onClick={handleClick}>1</button>
+        <button onClick={handleClick}>2</button>
+        <button onClick={handleClick}>3</button>
+        <button onClick={handleClick}>4</button>
+        <button onClick={handleClick}>5</button>
         </div>
 
 
-        <button className="submit-btn">Submit</button>
+        <button className="submit-btn" onClick={handleSubmit}>Submit</button>
     </section>
   );
 }
